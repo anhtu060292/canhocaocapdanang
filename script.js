5bdd433fec5e4e6e3c4c4e3066771b491bf1eb97
anhtu060292 <tanganhtu18@gmail.com>
2025-03-22 19:00:47 +0700

    Cập nhật trang landing page

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+ // Hàm để mở/đóng menu
+ function toggleMenu() {
+  var menu = document.getElementById('menu'); // Lấy đối tượng menu
+  // Thêm hoặc bỏ lớp 'active' để hiển thị/ẩn menu
+  menu.classList.toggle('active');
+}
+// Hàm để đóng menu nếu nhấn ra ngoài menu
+document.addEventListener('click', function(event) {
+  var menu = document.getElementById('menu');
+  var menuButton = document.querySelector('.menu-btn');
+
+  // Kiểm tra nếu người dùng nhấn ngoài menu và nút hamburger
+  if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+    menu.classList.remove('active');
+  }
+});
